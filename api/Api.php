173639df944ceaef730d720e1840731e98822b55
Unknown <emre.ozturk@student.pxl.be>
2018-05-30 01:11:32 +0200

    5 hours of searching Restfull API --> Working CRUD

---
+++ api/Api.php
@@ -0,0 +1,140 @@
+<?php
+
+//Api.php
+
+class API
+{
+	private $connect = '';
+
+	function __construct()
+	{
+		$this->database_connection();
+	}
+
+	function database_connection()
+	{
+		$this->connect = new PDO("mysql:host=localhost;dbname=pe4", "root", "");
+	}
+
+	function fetch_all()
+	{
+		$query = "SELECT * FROM locaties ORDER BY id";
+		$statement = $this->connect->prepare($query);
+		if($statement->execute())
+		{
+			while($row = $statement->fetch(PDO::FETCH_ASSOC))
+			{
+				$data[] = $row;
+			}
+			return $data;
+		}
+	}
+
+	function insert()
+	{
+		if(isset($_POST["gemeente"]))
+		{
+			$form_data = array(
+				':gemeente'		=>	$_POST["gemeente"],
+				':postcode'		=>	$_POST["postcode"]
+			);
+			$query = "
+			INSERT INTO locaties 
+			(gemeente, postcode) VALUES 
+			(:gemeente, :postcode)
+			";
+			$statement = $this->connect->prepare($query);
+			if($statement->execute($form_data))
+			{
+				$data[] = array(
+					'success'	=>	'1'
+				);
+			}
+			else
+			{
+				$data[] = array(
+					'success'	=>	'0'
+				);
+			}
+		}
+		else
+		{
+			$data[] = array(
+				'success'	=>	'0'
+			);
+		}
+		return $data;
+	}
+
+	function fetch_single($id)
+	{
+		$query = "SELECT * FROM locaties WHERE id='".$id."'";
+		$statement = $this->connect->prepare($query);
+		if($statement->execute())
+		{
+			foreach($statement->fetchAll() as $row)
+			{
+				$data['gemeente'] = $row['gemeente'];
+				$data['postcode'] = $row['postcode'];
+			}
+			return $data;
+		}
+	}
+
+	function update()
+	{
+		if(isset($_POST["gemeente"]))
+		{
+			$form_data = array(
+				':gemeente'	=>	$_POST['gemeente'],
+				':postcode'	=>	$_POST['postcode'],
+				':id'			=>	$_POST['id']
+			);
+			$query = "
+			UPDATE locaties 
+			SET gemeente = :gemeente, postcode = :postcode 
+			WHERE id = :id
+			";
+			$statement = $this->connect->prepare($query);
+			if($statement->execute($form_data))
+			{
+				$data[] = array(
+					'success'	=>	'1'
+				);
+			}
+			else
+			{
+				$data[] = array(
+					'success'	=>	'0'
+				);
+			}
+		}
+		else
+		{
+			$data[] = array(
+				'success'	=>	'0'
+			);
+		}
+		return $data;
+	}
+	function delete($id)
+	{
+		$query = "DELETE FROM locaties WHERE id = '".$id."'";
+		$statement = $this->connect->prepare($query);
+		if($statement->execute())
+		{
+			$data[] = array(
+				'success'	=>	'1'
+			);
+		}
+		else
+		{
+			$data[] = array(
+				'success'	=>	'0'
+			);
+		}
+		return $data;
+	}
+}
+
+?>
---
+++ work/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Rest API PE4</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<br />
+			
+			<h3 align="center">Rest API PE4</h3>
+			<br />
+			<div align="right" style="margin-bottom:5px;">
+				<button type="button" name="add_button" id="add_button" class="btn btn-success btn-xs">Add</button>
+			</div>
+
+			<div class="table-responsive">
+				<table class="table table-bordered table-striped">
+					<thead>
+						<tr>
+							<th>gemeente</th>
+							<th>postcode</th>
+							<th>Edit</th>
+							<th>Delete</th>
+						</tr>
+					</thead>
+					<tbody></tbody>
+				</table>
+			</div>
+		</div>
+	</body>
+</html>
+
+<div id="apicrudModal" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form method="post" id="api_crud_form">
+				<div class="modal-header">
+		        	<button type="button" class="close" data-dismiss="modal">&times;</button>
+		        	<h4 class="modal-title">Add Data</h4>
+		      	</div>
+		      	<div class="modal-body">
+		      		<div class="form-group">
+			        	<label>Enter gemeente</label>
+			        	<input type="text" name="gemeente" id="gemeente" class="form-control" />
+			        </div>
+			        <div class="form-group">
+			        	<label>Enter postcode</label>
+			        	<input type="text" name="postcode" id="postcode" class="form-control" />
+			        </div>
+			    </div>
+			    <div class="modal-footer">
+			    	<input type="hidden" name="hidden_id" id="hidden_id" />
+			    	<input type="hidden" name="action" id="action" value="insert" />
+			    	<input type="submit" name="button_action" id="button_action" class="btn btn-info" value="Insert" />
+			    	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	      		</div>
+			</form>
+		</div>
+  	</div>
+</div>
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+
+	fetch_data();
+
+	function fetch_data()
+	{
+		$.ajax({
+			url:"fetch.php",
+			success:function(data)
+			{
+				$('tbody').html(data);
+			}
+		})
+	}
+
+	$('#add_button').click(function(){
+		$('#action').val('insert');
+		$('#button_action').val('Insert');
+		$('.modal-title').text('Add Data');
+		$('#apicrudModal').modal('show');
+	});
+
+	$('#api_crud_form').on('submit', function(event){
+		event.preventDefault();
+		if($('#gemeente').val() == '')
+		{
+			alert("Enter gemeente");
+		}
+		else if($('#postcode').val() == '')
+		{
+			alert("Enter postcode");
+		}
+		else
+		{
+			var form_data = $(this).serialize();
+			$.ajax({
+				url:"action.php",
+				method:"POST",
+				data:form_data,
+				success:function(data)
+				{
+					fetch_data();
+					$('#api_crud_form')[0].reset();
+					$('#apicrudModal').modal('hide');
+					if(data == 'insert')
+					{
+						alert("locatie added!");
+					}
+					if(data == 'update')
+					{
+						alert("Locatie gewijzigd");
+					}
+				}
+			});
+		}
+	});
+
+	$(document).on('click', '.edit', function(){
+		var id = $(this).attr('id');
+		var action = 'fetch_single';
+		$.ajax({
+			url:"action.php",
+			method:"POST",
+			data:{id:id, action:action},
+			dataType:"json",
+			success:function(data)
+			{
+				$('#hidden_id').val(id);
+				$('#gemeente').val(data.gemeente);
+				$('#postcode').val(data.postcode);
+				$('#action').val('update');
+				$('#button_action').val('Update');
+				$('.modal-title').text('Edit Data');
+				$('#apicrudModal').modal('show');
+			}
+		})
+	});
+
+	$(document).on('click', '.delete', function(){
+		var id = $(this).attr("id");
+		var action = 'delete';
+		if(confirm("Bent u zeker om de locatie te verwijderen?"))
+		{
+			$.ajax({
+				url:"action.php",
+				method:"POST",
+				data:{id:id, action:action},
+				success:function(data)
+				{
+					fetch_data();
+					alert("Locatie verwijderd");
+				}
+			});
+		}
+	});
+
+});
+</script>
